391ceefedbeecf52eaa1ad4b9f83877c19771cc6
Joaquin Bruhn <joaquin_bruhn@hotmail.com.ar>
2022-10-31 17:17:30 -0300

    merging with remote

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 function Navigation() {
-  return (
-    <div className='text-3xl font-bold underline'>Navigation</div>
-  )
+  return <div className="text-3xl font-bold underline">Navigation!!!</div>;
 }
 
-export default Navigation
+export default Navigation;
